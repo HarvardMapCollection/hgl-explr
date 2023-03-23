dbb16cd95a509e563ae8ff3acde29f8cbe316acb
Belle Lipton <belle_lipton@harvard.edu>
2023-03-23 18:03:18 -0400

    add description dates

--- scripts/get-jsons.py
+++ scripts/get-jsons.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 path_to_json = '{FILEPATH}/harvard-geodata/json'
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
-jsons_data = pd.DataFrame(columns=['layerid','title', 'description', 'layertype', 'creator', 'format', 'date', 'place', 'subject', 'bbox'])
+jsons_data = pd.DataFrame(columns=['layerid','title', 'description', 'layertype', 'creator', 'format', 'date', 'desc_date', 'place', 'subject', 'bbox'])
 
 for index, js in enumerate(json_files):
     with open(os.path.join(path_to_json, js)) as json_file:
@@ -20,13 +20,14 @@ for index, js in enumerate(json_files):
             date = json_text['solr_year_i']
         except KeyError:
             date = "null"
+        desc_date = json_text['layer_modified_dt']
         try:
             place = json_text['dct_spatial_sm'][0]
         except KeyError:
             place = "null"
         subject = json_text['dc_subject_sm'][0]
         bbox = json_text['solr_geom']
-        jsons_data.loc[index] = [layerid, title, description, layertype, creator, format, date, place, subject, bbox]
+        jsons_data.loc[index] = [layerid, title, description, layertype, creator, format, date, desc_date, place, subject, bbox]
 
 # now that we have the pertinent json data in our DataFrame let's look at it
 jsons_data.to_csv('{FILEPATH}/hgl-records-summary.csv')
